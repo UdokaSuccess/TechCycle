ba3447ad7aa36dbf735d8b3c45ae07c43492ff51
TosinMary <teetoastl3@gmail.com>
2024-04-08 01:28:00 +0100

    file checked

--- tech-cycle/src/pages/ContactUs.js
+++ tech-cycle/src/pages/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactBody from './ContactBody'
 import Footer from "../pages/Footer"
+import Navbar from '../Navbar'
 
 function ContactUs() {
   return (
     <section>
+    <Navbar />
  <div className='contact-hero'>
     <p>Contact T-Cycle</p>
  </div>
